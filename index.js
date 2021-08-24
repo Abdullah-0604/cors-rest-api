5169be59757d473089b0ebab99437fc3bb0fe2d7
Abdullah-0604 <tmilan0604@gmail.com>
2021-08-24 21:34:57 +0545

    cors api

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const cors = require('cors');
+const app = express()
+app.use(cors())
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+//cors api
+app.get('/todays-price', (req, res) => {
+    // console.log('asdf');
+     fetch('https://nepse-data-api.herokuapp.com/data/todaysprice').then(res => res.json()).then(text => {
+         res.json(text);
+     }).catch(err => {
+         res.status(400).send('failed to fetch');
+     });
+ });
+ 
+
+app.listen(port, () => {
+  console.log(`Server app listening at http://localhost:${port}`)
+})
